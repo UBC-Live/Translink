2860d8c0c842d32d322a3139181d515d3c272a4f
Toby <toby.lau075@gmail.com>
2025-11-23 03:57:53 -0800

    delete run all file

--- scripts/run_all.py
+++
@@ -1,7 +0,0 @@
-from fetch_realtime import run as fetch_realtime
-from fetch_static import run as fetch_static
-from datetime import datetime
-
-now = datetime.now().strftime("%Y-%m-%dT%H-%M")
-fetch_static(now)
-fetch_realtime(now)
